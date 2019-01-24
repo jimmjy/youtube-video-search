214d87049fcf951c79510b3347c2f389de140bf1
James Finkelstein <jamesfinkelstein@Jamess-MacBook.local>
2019-01-24 14:49:08 -0500

    Updates

---
+++ precache-manifest.a6801d1c6e20677e2aa30d6488cd4a34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b62bead750fee65d6f5",
+    "url": "/youtube-video-search/static/js/runtime~main.7b62bead.js"
+  },
+  {
+    "revision": "1e74f799f5e6436be967",
+    "url": "/youtube-video-search/static/js/main.1e74f799.chunk.js"
+  },
+  {
+    "revision": "02ba3405e7481ccc0a43",
+    "url": "/youtube-video-search/static/js/1.02ba3405.chunk.js"
+  },
+  {
+    "revision": "1e74f799f5e6436be967",
+    "url": "/youtube-video-search/static/css/main.7aeff8a2.chunk.css"
+  },
+  {
+    "revision": "9876929774fd199e4ca7671b155fdeac",
+    "url": "/youtube-video-search/index.html"
+  }
+];
